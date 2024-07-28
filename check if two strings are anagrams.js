312a07e2bb583db3018ee9699d651fe1cf575b0a
TheMan <126444531+Gurureddy006@users.noreply.github.com>
2024-07-28 16:38:12 +0530

    Added anagram program
    Checking if two strings are anagrams.

---
+++ check if two strings are anagrams.js
@@ -0,0 +1,18 @@
+// Write the code given If two strings are anagrams of one another, then return true.
+
+var str1 = "Guru";
+var str2 = "Shiva";
+
+console.log(isAnagram(str1,str2));
+
+function isAnagram(one,two){
+    var a = one.toLowerCase();
+    var b = two.toLowerCase();
+
+    if (a.length != b.length){
+        return false;
+    }
+    a = a.split("").sort().join("");
+    b = b.split("").sort().join("");
+    return a === b;
+}
