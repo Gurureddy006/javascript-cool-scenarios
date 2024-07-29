f78f150e0f53bda310d49f3de63e9db6d13363f2
TheMan <126444531+Gurureddy006@users.noreply.github.com>
2024-07-29 15:42:24 +0530

    Update and rename guess the output.js to guess the output .js

--- guess the output .js
+++ guess the output .js
@@ -6,10 +6,10 @@
     return (function(){
       console.log(a); // a = 45
       a = 23;
-      console.log(a)  //a = 23
+      // console.log(a)  //a = 23
     })()
 })(45);
 
 // In the outer iife, function has parameter a that takes 45 as an argument.
-// Inner iife has no parameters so the immediate console.log(a) stores 45.
-// After logging then the value of a will be assigned with 23. If we have another console.log after assigning with 23 then value of a will be 23.
+// Inner iife has no parameters so the immediate console.log(a) stores 45 which inturn returns a = 45.
+// After logging, the value of a will be assigned with 23. If we have another console.log after assigning with 23 then value of a will be 23.
