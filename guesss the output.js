c7df4f7ae5ef6059097075781cbaa57675e133f3
TheMan <126444531+Gurureddy006@users.noreply.github.com>
2024-07-30 15:31:15 +0530

    Adding a general program

--- guesss the output.js
+++ guesss the output.js
@@ -1,8 +1,8 @@
-function runFunc(){
-    console.log("1" + 1);
-    console.log("A" - 1);
-    console.log(2 + "-2" + "2");
-    console.log("Hello" - "World" + 78);
-    console.log("Hello"+ "78");
-}
-runFunc();
+let a = 0;
+let b = false;
+console.log((a == b)); 
+console.log((a === b));
+
+// Here we are using two == once and then === signs
+// Two == means loose equality which has number and Boolean, here b = false will be converted by javascript to a number which is 0 then this returns true as 0 == 0
+// Three === means strict equality, since the types of a and b are different it will check 0 === false which is false because both are not same 
