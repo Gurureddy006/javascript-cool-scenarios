5fc340ab023c6cd62c954b27bdff06b073c30455
TheMan <126444531+Gurureddy006@users.noreply.github.com>
2024-07-30 16:13:29 +0530

    Update guesss the output.js

--- guesss the output.js
+++ guesss the output.js
@@ -3,6 +3,7 @@ let b = false;
 console.log((a == b)); 
 console.log((a === b));
 
-// Here we are using two == once and then === signs
-// Two == means loose equality which has number and Boolean, here b = false will be converted by javascript to a number which is 0 then this returns true as 0 == 0
-// Three === means strict equality, since the types of a and b are different it will check 0 === false which is false because both are not same 
+// Here we are using two == once and then === signs.
+// Two == means loose equality, here b = false will be converted by javascript to a number which is 0 then this returns true as 0 == 0.
+// In general, we mostly consider true = 1 and false = 0 
+// Three === means strict equality, since the types of a and b are different it will check 0 === false which is false because both are not same.
