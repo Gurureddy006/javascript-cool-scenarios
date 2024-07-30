a19e962e2620b7b28b5a43fc30cdcc13e7a835e1
TheMan <126444531+Gurureddy006@users.noreply.github.com>
2024-07-30 15:43:49 +0530

    Added a program which has func inside a func
    In this a settimeout function is kept inside a function and checking the output.

---
+++ guess.js
@@ -0,0 +1,16 @@
+// setTimeout function inside a function
+
+function func1(){
+    setTimeout(()=>{
+      console.log(x);
+      console.log(y);
+    },3000);
+  
+    var x = 2;
+    let y = 12;
+}
+func1();
+
+// At first sight everyone will think that console.log will return nan, I also thought the same at first
+// But here the entire function runs before entering setTimeout function. So the function has access to x and y variables
+// Now everyone can guess the output :) 
