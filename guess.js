f95cb3922407789e67c7a7f638d1b98a6092c8c3
TheMan <126444531+Gurureddy006@users.noreply.github.com>
2024-07-30 16:11:45 +0530

    Update guess.js

--- guess.js
+++ guess.js
@@ -12,5 +12,5 @@ function func1(){
 func1();
 
 // At first sight everyone will think that console.log will return nan, I also thought the same at first
-// But here the entire function runs before entering setTimeout function. So the function has access to x and y variables
+// But here the entire function runs before entering setTimeout function. So the function has access to x and y variables and their values.
 // Now everyone can guess the output :) 
